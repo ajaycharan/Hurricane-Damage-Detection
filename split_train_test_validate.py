6396dace63c69155d3d30074517599aa2043a37a
Sandeep Dcunha <sdcunha@umass.edu>
2017-10-05 09:41:58 -0400

    Fixed parantheses bug

--- split_train_test_validate.py
+++ split_train_test_validate.py
@@ -1,5 +1,4 @@
-import os
-import random
+import osimport random
 
 images = [f for f in os.listdir('.') if f.endswith(".png")]
 random.shuffle(images)
@@ -12,6 +11,6 @@ validation = train_val[int(0.8*len(train_val)):]
 for set, fname in zip([train, test, validation], ["training", "testing", "validation"]):
     for f in set:
         os.renames(f, os.path.join('.', fname, os.path.basename(f)))
-        os.renames(os.path.join("annotations", "label_" + f, os.path.join("annotations", fname, "label_" + os.path.basename(f))))
+        os.renames(os.path.join("annotations", "label_" + f), os.path.join("annotations", fname, "label_" + os.path.basename(f)))
 
     
