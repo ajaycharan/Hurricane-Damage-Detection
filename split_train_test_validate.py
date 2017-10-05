997d7fb7949a0beb109c27826e5ec6f0f56af445
Sandeep Dcunha <sdcunha@umass.edu>
2017-10-05 11:22:47 -0400

    Fixed directory structure change bug

--- split_train_test_validate.py
+++ split_train_test_validate.py
@@ -1,7 +1,9 @@
 import os
 import random
-
-images = [f for f in os.listdir('.') if f.endswith(".png")]
+image_dir = "images"
+annotation_dir = "annotations"
+annotation_prefix = "label_"
+images = [f for f in os.listdir('images') if f.endswith(".png")]
 random.shuffle(images)
 
 train_val = images[:int(0.8*len(images))]
@@ -11,7 +13,7 @@ validation = train_val[int(0.8*len(train_val)):]
 
 for set, fname in zip([train, test, validation], ["training", "testing", "validation"]):
     for f in set:
-        os.renames(f, os.path.join('.', "images", fname, os.path.basename(f)))
-        os.renames(os.path.join("annotations", "label_" + f), os.path.join("annotations", fname, "label_" + os.path.basename(f)))
+        os.renames(os.path.join(image_dir, f), os.path.join(image_dir, fname, os.path.basename(f)))
+        os.renames(os.path.join(annotation_dir, annotation_prefix + f), os.path.join(annotation_dir, fname, annotation_prefix + os.path.basename(f)))
 
     
