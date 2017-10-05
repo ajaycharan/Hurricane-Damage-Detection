4d02184ee43600dcd29a5b8e105ec143a44c8018
Sandeep Dcunha <sdcunha@umass.edu>
2017-10-05 11:11:59 -0400

    Changed image splitting folder format

--- split_train_test_validate.py
+++ split_train_test_validate.py
@@ -11,7 +11,7 @@ validation = train_val[int(0.8*len(train_val)):]
 
 for set, fname in zip([train, test, validation], ["training", "testing", "validation"]):
     for f in set:
-        os.renames(f, os.path.join('.', fname, os.path.basename(f)))
+        os.renames(f, os.path.join('.', "images", fname, os.path.basename(f)))
         os.renames(os.path.join("annotations", "label_" + f), os.path.join("annotations", fname, "label_" + os.path.basename(f)))
 
     
